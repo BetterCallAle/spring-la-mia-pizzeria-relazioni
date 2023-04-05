eba5af35939cc6cc93201620e1f8ee74164791b4
BetterCallAle <dap22091998@gmail.com>
2023-04-05 17:42:50 +0200

    updated import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 INSERT INTO pizzas (name,  description, price) VALUES('Margherita', 'Pizza base', 4.00);
 INSERT INTO pizzas (name,  description, price) VALUES('Quattro Formaggi', 'Pizza ai quattro formaggi', 8.00);
 INSERT INTO pizzas (name,  description, price) VALUES('Crudaiola', 'Pizza alla crudaiola', 5.00);
+INSERT INTO special_offers (title,  starting_date, ending_date, pizza_id) VALUES('Sconto del 20%', '2023-04-05', '2023-04-06', 1);
+INSERT INTO special_offers (title,  starting_date, ending_date, pizza_id) VALUES('Sconto del 30%', '2023-04-07', '2023-04-20', 2);
