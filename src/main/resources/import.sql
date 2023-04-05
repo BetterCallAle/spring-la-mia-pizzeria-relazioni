c442ec759ca3353c50d05b534beb9938c182cbcd
BetterCallAle <dap22091998@gmail.com>
2023-04-05 14:09:20 +0200

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO pizzas (name,  description, price) VALUES('Margherita', 'Pizza base', 4.00);
+INSERT INTO pizzas (name,  description, price) VALUES('Quattro Formaggi', 'Pizza ai quattro formaggi', 8.00);
+INSERT INTO pizzas (name,  description, price) VALUES('Crudaiola', 'Pizza alla crudaiola', 5.00);
